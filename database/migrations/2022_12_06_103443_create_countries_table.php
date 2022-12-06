82b8c15fc0b30f78a46db70349c041fbbaa8691c
Hassane Dao <hassane.dao@paymetrust.net>
2022-12-06 10:38:06 +0000

    feat: country model and migration

---
+++ database/migrations/2022_12_06_103443_create_countries_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->string('name');
+            $table->string('iso_code');
+            $table->string('dial_code');
+            $table->string('mask');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('countries');
+    }
+};
