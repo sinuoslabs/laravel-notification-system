d2671228cc3b5cb9f158ff3e1ada9fd3afae1753
Hassane Dao <hassane.dao@paymetrust.net>
2022-12-10 21:06:36 +0000

    refactor: channel enum

--- src/Module/Core/Domain/Entities/Settings.php
+++ src/Module/Core/Domain/Entities/Settings.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Domain\Entities;
 
-use Domain\Channel;
+use Domain\Enums\Channel;
 
 class Settings
 {
--- src/Module/Core/Domain/Enums/Channel.php
+++ src/Module/Core/Domain/Enums/Channel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Domain;
+namespace Domain\Enums;
 
 enum Channel: string
 {
