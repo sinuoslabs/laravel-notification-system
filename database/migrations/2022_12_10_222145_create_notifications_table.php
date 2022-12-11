0f939cfb330cb031dd5d2d7780ace6dd2b6e0b30
Houssene Dao <dao.houssene@gmail.com>
2022-12-11 00:58:19 +0000

    refactor: notification migration

--- database/migrations/2022_12_10_222145_create_notifications_table.php
+++ database/migrations/2022_12_10_222145_create_notifications_table.php
@@ -10,8 +10,18 @@ return new class () extends Migration {
     public function up()
     {
         Schema::create('notifications', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
+            $table->uuid('user_id')->index();
+            $table->string('notification_type')->index();
+            $table->string('channel')->index();
+            $table->text('payload');
+            $table->enum('status', ['pending', 'processing', 'failed', 'sent']);
+            $table->dateTime('processing_at')->nullable();
+            $table->dateTime('read_at')->nullable();
+            $table->dateTime('delivered_at')->nullable();
             $table->timestamps();
+
+            $table->index(['user_id', 'status', 'notification_type', 'delivered_at', 'processing_at']);
         });
     }
 
